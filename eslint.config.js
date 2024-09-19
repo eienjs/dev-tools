f5d01c88e94124f049ae83bcf23246b1f22224ec
Fernando IL <luffynando@gmail.com>
2024-09-19 14:30:35 -0600

    fix: :bug: resolve problem with registry macros of api response

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import nodecfdiConfig from '@nodecfdi/eslint-config';
 const { defineConfig } = nodecfdiConfig(import.meta.dirname);
 
 export default defineConfig({
-  files: ['src/providers/*.ts', 'src/plugins/japa/api_client.ts'],
+  files: ['src/providers/*.ts', 'src/plugins/japa/dev_tools_api_client.ts'],
   rules: {
     '@typescript-eslint/consistent-type-definitions': 'off',
   },
